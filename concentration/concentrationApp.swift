44eb96c3bc358437b39059d623dc34f3c2d811b8
Carly Nield <nieldcarly@gmail.com>
2020-09-09 17:12:40 -0600

    Initial Commit

---
+++ concentration/concentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  concentrationApp.swift
+//  concentration
+//
+//  Created by Carly Nield on 9/9/20.
+//
+
+import SwiftUI
+
+@main
+struct concentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
